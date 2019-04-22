5f7f30305e65264aa4a0eba9218ab4b915ac59ab
Infinity <kreatorspace@gmail.com>
2019-04-22 09:42:48 -0700

    Animation tolls added

---
+++ animation/tools.py
@@ -0,0 +1,33 @@
+def swap_key_values():
+    objs = cmds.ls(sl=True)
+    a = objs[0]
+    b = objs[1]
+    atx = cmds.getAttr(a + '.tx')
+    aty = cmds.getAttr(a + '.ty')
+    atz = cmds.getAttr(a + '.tz')
+    arx = cmds.getAttr(a + '.rx')
+    ary = cmds.getAttr(a + '.ry')
+    arz = cmds.getAttr(a + '.rz')
+
+    btx = cmds.getAttr(b + '.tx')
+    bty = cmds.getAttr(b + '.ty')
+    btz = cmds.getAttr(b + '.tz')
+    brx = cmds.getAttr(b + '.rx')
+    bry = cmds.getAttr(b + '.ry')
+    brz = cmds.getAttr(b + '.rz')
+
+    cmds.setAttr(a + '.tx',btx)
+    cmds.setAttr(a + '.ty',bty)
+    cmds.setAttr(a + '.tz',btz)
+    cmds.setAttr(a + '.rx',brx)
+    cmds.setAttr(a + '.ry',bry)
+    cmds.setAttr(a + '.rz',brz)
+
+    cmds.setAttr(b + '.tx',atx)
+    cmds.setAttr(b + '.ty',aty)
+    cmds.setAttr(b + '.tz',atz)
+    cmds.setAttr(b + '.rx',arx)
+    cmds.setAttr(b + '.ry',ary)
+    cmds.setAttr(b + '.rz',arz)
+
+    print("Done key values swap")
--- rigging/rigging_tools.py
+++ rigging/rigging_tools.py
@@ -9,26 +9,90 @@ def createRiggingToolsUI():
     if cmds.window( windowID, exists=True ):
         cmds.deleteUI( windowID )
         
-    cmds.window( windowID, title="MCAKE: Rigging Tools", sizeable=False, resizeToFitChildren=True )
+    cmds.window( windowID, title="MCAKE: Animation & Rigging Tools", sizeable=False, resizeToFitChildren=True )
     
-    cmds.rowColumnLayout( numberOfColumns=3, columnWidth=[ (1,120), (2,120), (3,120) ], columnOffset=[ (1,'right',3) ] )
+    cmds.rowColumnLayout( numberOfColumns=3, columnWidth=[ (1,120), (2,120), (3,120) ], columnOffset=[ ] )
     
-    def applyCallback( pStartTimeField, pEndTimeField, pTargetAttributeField, *pArgs ):
-        print('Apply button pressed.')
-    
-    cmds.button( label='Mirror Rot Trans [y,z]', command=eh_mirror_rot_trans )
-    cmds.button( label='Mirror Rot', command=eh_mirror_rot )
+    cmds.text( label='Attribute Name:' )
+    cnFld = cmds.textField( text='UrvaX2' )
+    cmds.separator( h=10, style='none' )
+
+    cmds.button( label='Mirror Rot Trans', command=functools.partial(eh_copy_vals, ['rx','ry','rz','tx','ty','tz'] ))
+    cmds.button( label='Mirror Given', command=functools.partial(eh_mirror_given, cnFld))
+    cmds.separator( h=10, style='none' )
+    cmds.button( label='Mirror rx', command=functools.partial(eh_copy_vals, ['rx'] ))
+    cmds.button( label='Mirror ry', command=functools.partial(eh_copy_vals, ['ry'] ))
+    cmds.button( label='Mirror rz', command=functools.partial(eh_copy_vals, ['rz'] ))
+    cmds.button( label='Mirror tx', command=functools.partial(eh_copy_vals, ['tx'] ))
+    cmds.button( label='Mirror ty', command=functools.partial(eh_copy_vals, ['ty'] ))
+    cmds.button( label='Mirror tz', command=functools.partial(eh_copy_vals, ['tz'] ))
+
+
+    cmds.button( label='Copy Rot Trans', command=functools.partial(eh_mirror_vals, ['rx','ry','rz','tx','ty','tz'] ))
+    cmds.button( label='Copy Given', command=functools.partial(eh_copy_given, cnFld))
+    cmds.separator( h=10, style='none' )
+    cmds.button( label='Copy rx', command=functools.partial(eh_mirror_vals, ['rx'] ))
+    cmds.button( label='Copy ry', command=functools.partial(eh_mirror_vals, ['ry'] ))
+    cmds.button( label='Copy rz', command=functools.partial(eh_mirror_vals, ['rz'] ))
+    cmds.button( label='Copy tx', command=functools.partial(eh_mirror_vals, ['tx'] ))
+    cmds.button( label='Copy ty', command=functools.partial(eh_mirror_vals, ['ty'] ))
+    cmds.button( label='Copy tz', command=functools.partial(eh_mirror_vals, ['tz'] ))
     
     # Cancel button
     cmds.button( label='Cancel', command=functools.partial( g_cancelCallback, windowID))
 
     cmds.showWindow()
 
-
+# Event Handlers
 def g_cancelCallback(windowID,  *pArgs ):
     if cmds.window( windowID, exists=True ):
         cmds.deleteUI( windowID )
 
+def eh_copy_given(fld):
+    eh_copy_vals(uu.text_val(cnFld))
+
+def eh_mirror_given(fld):
+    eh_mirror_vals(uu.text_val(cnFld))
+
+# copy([rx,ry,rz])
+def eh_copy_vals(attrs):
+    objs = cmds.ls(sl=True)
+    if len(objs) == 0:
+        return
+
+    a = objs[0]
+    b = objs[1]
+    avals = []
+    bvals = []
+
+    for attr in attrs:
+        ta = cmds.getAttr(a + '.' + attr)
+        tb = cmds.getAttr(b + '.' + attr)
+        cmds.setAttr(a + '.' + attr, tb)
+        cmds.setAttr(b + '.' + attr, ta)
+
+    print("Done key value swap for", attrs)
+
+def eh_mirror_vals(attrs):
+    objs = cmds.ls(sl=True)
+    if len(objs) == 0:
+        return
+
+    a = objs[0]
+    b = objs[1]
+    avals = []
+    bvals = []
+
+    for attr in attrs:
+        ta = cmds.getAttr(a + '.' + attr)
+        tb = cmds.getAttr(b + '.' + attr)
+        cmds.setAttr(a + '.' + attr, -tb)
+        cmds.setAttr(b + '.' + attr, -ta)
+
+    print("Done key value mirror for", attrs)
+
+
+'''
 # Event Handler
 def eh_mirror_rot_trans(*pArgs):
     print("Mirror Keyable")
@@ -113,3 +177,4 @@ def eh_mirror_rot(*pArgs):
             print("Copied to " + obj)
         c += 1
     print("Mirror Keyable - Done")
+'''
